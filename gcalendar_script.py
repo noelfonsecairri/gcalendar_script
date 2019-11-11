91c4f6dead07247033331947079d3258101d2813
noelfonsecairri <n.fonseca@irri.org>
2019-11-11 16:04:23 +0800

    modified gcalendar script, added pickle, etc

--- gcalendar_script.py
+++ gcalendar_script.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 
+import pprint, pickle, json
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-service = build('calendar', 'v3')
+scopes = ["https://www.googleapis.com/auth/calendar"]
+flow = InstalledAppFlow.from_client_secrets_file(
+    "irri_google_calendar_credentials.json", scopes=scopes
+)
+credentials = flow.run_local_server()
 
 
-collection = service.stamps()
+pickle.dump(credentials, open("token.pkl", "wb"))
+credentials = pickle.load(open("token.pkl", "rb"))
+service = build("calendar", "v3", credentials=credentials)
 
-request = collection.list(cents = 5)
-print(request)
+result = service.calendarList().list().execute()
+# pprint.pprint(result['items'][0])
+
+calendar_id = result["items"][0]["id"]
+result = service.events().list(calendarId=calendar_id, timeZone="Asia/Manila").execute()
+
+
+my_data = result["items"]
+
+
+pprint.pprint(my_data[100])
+
+
+
+
+for x in my_data:
+    if my_data[x]['attendees']['email'] == 'l.delosreyes@irri.org':
+        print(x['description'])
+
+"""    for i in x['attendees']:
+        if i['email'] == 'l.delosreyes@irri.org':
+            print(my_data[x]['description'])"""
+             
